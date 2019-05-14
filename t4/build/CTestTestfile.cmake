4fd336e64a578d4ee93c7b5b77879b3c03b87d05
sunmiaozju <sun_miao@zju.edu.cn>
2019-05-14 10:36:45 +0800

    first commit

---
+++ t4/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sunm/work/code/cmake-learn/t4
+# Build directory: /home/sunm/work/code/cmake-learn/t4/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(mytest "/home/sunm/work/code/cmake-learn/t4/build/src/main")
+subdirs(src)
